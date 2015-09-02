3bf0158bab6441eee393484541f99f659bb44b46
junaidi.jusoh@daimler.com <junaidi.jusoh@daimler.com>
2015-09-02 12:00:03 +0800

    Added secret page.

---
+++ app/secret/app.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+  angular.module('app', [ 
+    'ngRoute', 
+    'ngSanitize', 
+    'xeditable', 
+    'ui.bootstrap', 
+    'duScroll', 
+    'app.directives', 
+    'app.cache', 
+    'app.data', 
+    'app.storage', 
+    'app.theme', 
+    'app.model' 
+  ]);
+})();
