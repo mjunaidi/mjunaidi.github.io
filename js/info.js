38dbd0851e1cbb53c5e7a8ef107fa6563b90185c
M Junaidi <mjunaidi@Ms-MacBook-Pro.local>
2018-05-13 15:40:07 +0800

    Free update, fix or changes.

---
+++ js/info.js
@@ -0,0 +1,229 @@
+const info = {
+  name: 'M Junaidi Jusoh',
+  details: {
+    email: 'junaidi.jusoh@daimler.com',
+    phone: '+6013-350 3789',
+    location: 'Selangor, Malaysia',
+    links: [
+      {
+        name: 'Online Portfolio',
+        link: 'https://mjunaidi.github.io',
+      },
+      {
+        name: 'GitHub Repositories',
+        link: 'https://github.com/mjunaidi',
+      },
+      {
+        name: 'CodePen',
+        link: 'https://codepen.io/mjunaidi/',
+      },
+      {
+        name: 'Blog',
+        link: 'https://www.techcycle.me'
+      }
+    ]
+  },
+  experience: [
+    {
+      company: {
+        name: 'Mercedes-Benz (Malaysia)',
+        location: 'Kinrara, Selangor',
+        link: 'http://www.mercedes-benz.com.my',
+      },
+      positions: [
+        {
+          name: 'Senior Software Engineer',
+          from: 'Jun 2015',
+          to: 'now',
+          details: [
+            'Working directly with Daimler TSS (Germany).',
+            'Responsible in development of a number of applications.',
+            [
+              'GSSN',
+              [
+                'A system used for managing dealer network data and the control of sales processes.',
+                [
+                  'Java', 'JSF', 'JavaScript', 'IBM’s Db2'
+                ],
+                'Translation Provider – A microservice to manage content languages used in other GSSN services. Is a part of the new GSSN called GSSN+.',
+                [
+                  'AngularJS', 'Java', 'IBM’s Db2', 'Docker', 'Kubernetes'
+                ]
+              ],
+            ],
+          ],
+        },
+      ],
+    },
+    {
+      company: {
+        name: 'BT (Malaysia)',
+        location: 'Bangsar South, KL',
+        link: 'http://www.btplc.com',
+        details: 'British multinational telecommunications holding company with head offices in London, UK. It has operations in around 180 countries and is the largest provider of fixed-line, mobile and broadband services in the UK',
+      },
+      positions: [
+        {
+          name: 'Software Engineer',
+          from: 'Sep 2014',
+          to: 'Jun 2015',
+          details: [
+            'Maintained batch processing application that handles data to and fro other applications.',
+            'Upgraded legacy systems to modern applications (Rest-API, web UI).',
+            'Designed and implemented service or change requests.',
+            'Practiced continuous integration TeamCity.',
+          ]
+        }
+      ],
+    },
+    {
+      company: {
+        name: 'Atex (Asia) Sdn Bhd'
+        link: 'http://www.atex.com/',
+        location: 'Damansara Heights, KL',
+        details: 'A leading software company specializes in creating solutions for media-rich industries. One of the products, Atex Web CMS is a platform that combines robust content management capabilities with audience-first features that help minimize business development lead times, support effortless cross-channel publishing, and enable rapid site creation.',
+      },
+      positions: [
+        {
+          name: 'Java Developer',
+          from: 'Sep 2011',
+          to: 'May 2014',
+          details: [
+            'Involved directly in product development of Atex Web CMS (the core engine) since 2012.',
+            'Lead Demo Project in 2013, developing a demo kit used in conference and by sales team.',
+            'Developed Mobile App Kit plugin for the CMS that can be used to turn the front end of the system into mobile-friendly site and mobile client app.',
+            'Managed integration and deployment using Jenkins.',
+            [
+              'Other projects',
+              [
+                {
+                  name: 'Agderposten',
+                  year: 2014,
+                  link: 'http://www.agderposten.no/',
+                  country: 'Norway',
+                },
+                {
+                  name: 'London24',
+                  year: 2013,
+                  link: 'http://www.london24.com/',
+                  country: 'UK',
+                },
+                {
+                  name: 'Ipswich Star',
+                  year: 2013,
+                  link: 'http://www.ipswichstar.co.uk/',
+                  country: 'UK',
+                },
+                {
+                  name: 'Great British Life',
+                  year: 2013,
+                  link: 'http://www.greatbritishlife.co.uk/',
+                  country: 'UK',
+                },
+                {
+                  name: 'Irish Times',
+                  year: 2012,
+                  link: 'http://www.irishtimes.com/',
+                  country: 'Ireland',
+                },
+                {
+                  name: 'Thompson',
+                  year: 2012,
+                  link: 'http://hr.complianceexpert.com/',
+                  country: 'USA',
+                },
+                {
+                  name: 'New Straits Times',
+                  year: 2012,
+                  link: 'http://www.nst.com.my/',
+                  country: 'Malaysia',
+                }
+              ],
+            ],
+          ]
+        }
+      ],
+      footer: 'Reason for leaving: Retrenchment',
+    },
+    {
+      company: {
+        name: 'Commerce Dot Com Sdn Bhn',
+        link: 'http://www.commercedc.com.my/',
+        location: 'Petaling Jaya, Selangor',
+        details: 'A public commerce G2B solutions provider that enables business transactions between privatised companies and the Government. Positioned as a mediator, its focal point is to serve as a catalyst between the two via the product – ePerolehan (an electronic procurement system).',
+      },
+      positions: [
+        {
+          name: 'Analyst Programmer',
+          from: 'Apr 2009',
+          to: 'Aug 2011',
+          details: [
+            'Developed web-based Java application (J2EE).',
+            'Created PL/SQL queries such as stored procedures and functions to fetch the business data to improve efficiency.',
+            'Programmed JavaScript for client side validation and drag-and-drop shopping cart to improve user experience (2009).',
+            'Projects; eCat 2.0 (an online catalog system with Web 2.0 features), Supplier Registration System, eBid (an online bidding system), and ePerolehan (an online procurement system that is currently used by the Government of Malaysia)'
+          ]
+        }
+      ],
+    },
+    {
+      company: {
+        name: 'FlairWork Technologies Sdn Bhd',
+        link: 'http://www.flairwork.com/',
+        location: 'KL',
+        details: 'A mobile and web solutions provider that creates services and reliable systems.',
+      },
+      positions: [
+        {
+          name: 'Software Engineer',
+          from 'Aug 2008',
+          to: 'Mar 2009',
+          details: [
+            'Develops a unique in-house framework using PHP, MySQL and AJAX that has been used in most client projects. The framework improves project implementation, reduces development effort and saves resources.',
+            'Projects; Organization Chart System for British American Tobacco/Group Delivery Service (Java, MySQL), Web Portal for Suruhanjaya Perkhidmatan Awam Perak (PHP), Corporate Website and Online Booking System for a travel agency (PHP, MySQL), Information Risk and Security Monitoring System for MAS (PHP, MySQL, AJAX), English-Malay online dictionary.',
+          ]
+        }
+      ]
+    },
+    {
+      company: {
+        name: 'Infosys Technologies Limited',
+        link: 'http://www.infosys.com/',
+        location: 'Bangalore, India'
+        details: 'A global leader in consulting, technology, and outsourcing solutions that help enterprises transform and thrive in a changing world, and co-creating breakthrough solutions, including those in mobility, sustainability, big data, and cloud computing.',
+      },
+      positions: [
+        {
+          name: 'Internship',
+          from: 'May',
+          to: 'Jul 2008',
+          details: [
+            'Trained on Enterprise Application Development mainly in Java and Oracle. Other subjects are HTML, Javascript, DBMS, quality systems, and project management.',
+            'Achievement; scored A in every training modules. (From total 100 people, only 2 scored all A\'s. I am one of the 2.)',
+          ],
+        }
+      ]
+    },
+    {
+      company: {
+        name: 'Skali Web Services Sdn Bhd',
+        link: 'http://www.skali.net',
+        location: 'Sentul, KL',
+        details: 'A technology company that provides infrastructure and solutions including the integral services needed to support the enterprise with its web objectives.'
+      },
+      positions: [
+        {
+          name: 'Software Developer',
+          from: 'Jul 2007',
+          to: 'Apr 2008',
+          details: [
+            'Lead a number of development project of web portals apart of 450 government agencies using JBoss portal, Java, PostgreSQL.',
+            'Developed a web-based Staff Directory System for the company using Java, Ajax and PostgreSQL.',
+            'Developed a web portal and web components for Young Professional Consultative Society (YPCS) using Joomla, PHP, javascript and mysql.',
+            'Developed a knowledge management portal with Document Management System (Web 2.0) using Neofission (company’s licensed CMS), PHP, Ajax and mysql.'
+          ]
+        }
+      ]
+    }
+  ]
+}
